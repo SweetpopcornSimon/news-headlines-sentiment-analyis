aba12413dabce3d08d0c4bc93f7730c39e7016fc
Simon Schroffner <simon@sweetpopcorn.studio>
2023-01-20 13:56:04 +0100

    Added more autoselectors

--- content_script.js
+++ content_script.js
@@ -2,34 +2,33 @@ function setup() {
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
             if(request.action === 'analyse_page') {
+                console.log('action analyse page');
                 analysePage(request.selector, (data) =>{
                     sendResponse( {message: 'done', data: data});
                 });
                 return true;
             } else if(request.action === 'highlight_page') {
-                hightlightPage(request.selector,(data) => {
-                    selector = request.selector ? request.selector : "h3";
-                    foundElements = document.getElementsByClassName(selector);
+                for(var i = 0; i < request.data.documents.length; i++) {
+                    item = request.data.documents[i];
+                    foundElements = document.getElementsByClassName(request.selector);
+                    el = foundElements[i];
+                    switch (item.sentiment) {
+                        case 'negative':
+                            el.style.backgroundColor  = 'red';
+                            //el.style.display = 'none';
+                            break;
+                        case 'positive':
+                            el.style.backgroundColor  = 'green';
+                            break;
+                        case 'neutral':
+                            el.style.backgroundColor  = 'yellow';
+                            break;
+                        default:
 
-                    for(var i = 0; i < data.results.documents.length; i++) {
-                        item = data.results.documents[i];
-                        el = foundElements[i];
-                        switch (item.sentiment) {
-                            case 'negative':
-                                el.style.backgroundColor  = 'red';
-                                //el.style.display = 'none';
-                                break;
-                            case 'positive':
-                                el.style.backgroundColor  = 'green';
-                                break;
-                            case 'neutral':
-                                el.style.backgroundColor  = 'yellow';
-                                break;
-                            default:
-
-                        }
                     }
-                    sendResponse( {message: 'done', data: data});
+                }
+                hightlightPage(request.selector, () => {
+                    sendResponse( {message: 'done'});
                 });
                 return true;
             } else if(request.action === 'unhighlight_page') {
@@ -50,13 +49,24 @@ function analysePage(selector, callback){
     allTitles = [];
     allElements = document.body.getElementsByClassName(selector);
 
-    for (let i = 0; i < 10; i++) {
-        el = allElements[i];
-        allTitles.push({
-            id: el.outerHTML,
-            text: el.innerText
-        });
+    if(allElements.length > 10) {
+        for (let i = 0; i < 10; i++) {
+            el = allElements[i];
+            allTitles.push({
+                id: el.outerHTML,
+                text: el.innerText
+            });
+        }
+    } else {
+        for (let el of allElements){
+            allTitles.push({
+                id: el.outerHTML,
+                text: el.innerText
+            });
+        }
     }
+
+
     chrome.runtime.sendMessage({contentScriptQuery: "analyseMultiple", allTitles: allTitles}, response => {
         if (response.message === 'success') {
             callback(response.data);
@@ -64,33 +74,16 @@ function analysePage(selector, callback){
     });
 }
 
+function hightlightPage(selector, callback) {
+    callback();
+}
+
 function unhightlightPage(selector, callback) {
     allElements = document.body.getElementsByClassName(selector);
     for (let item of allElements) {
         item.style.backgroundColor = '';
     }
-
     callback();
-}
-
-function hightlightPage(selector, callback) {
-    allElements = document.body.getElementsByClassName(selector);
-    myArray = [];
-    console.log('highlight page');
-
-    for (let i = 0; i < 10; i++) {
-        item = allElements[i];
-        myArray.push({
-            id: item.outerHTML,
-            text: item.innerText
-        });
-    }
-
-    chrome.runtime.sendMessage({contentScriptQuery: "analyseMultiple", allTitles: myArray}, response => {
-        if (response.message === 'success') {
-            callback(response.data);
-        }
-    });
 }
 
 
--- popup.js
+++ popup.js
@@ -2,17 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const element = document.querySelector("#checkPage");
 
     document.getElementById("customSelector").value = "";
+
+    autoFillSelector();
+
+
     element.addEventListener("click", () => {
         selector = document.getElementById("customSelector").value;
+        console.log('analyse', selector);
         analysePage(selector);
     });
 
-    const elementHighlightPage = document.querySelector("#highlightPage");
+   /*const elementHighlightPage = document.querySelector("#highlightPage");
 
     elementHighlightPage.addEventListener("click", () => {
         selector = document.getElementById("customSelector").value;
         hightlightPage(selector);
-    });
+    });*/
 
 
     const elementUnHighlightPage = document.querySelector("#unHighlightPage");
@@ -34,19 +39,40 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
-    const parsePage = document.querySelector("#testParse");
+    /*const parsePage = document.querySelector("#testParse");
 
     parsePage.addEventListener("click", () => {
         selector = document.getElementById("customSelector").value;
-        hightlightPage(selector);
         analysePage(selector);
-    });
+    });*/
 }, false);
 
-function hightlightPage(selector) {
+
+function autoFillSelector(){
+    let newsMap = new Map([
+        ["https://www.derstandard.at/", "teaser-title"],
+        ["https://edition.cnn.com/","cd__headline-text"],
+        ["https://www.nachrichten.at/","teaser__link"],
+        ["https://orf.at/", "ticker-story-headline"],
+        ["https://dietagespresse.com/", "entry-title"],
+        ["https://www.bbc.com/","media__link"],
+        ["https://www.oe24.at/","title"],
+        ["https://www.krone.at/","a__title"],
+        ["https://www.foxnews.com/", "title"],
+        ["https://www.nytimes.com/","indicate-hover"],
+        ["https://www.reuters.com/", "heading_5_half"],
+        ["https://techcrunch.com/", "post-block__title__link"]
+    ]);
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        var activeTab = tabs[0];
+        document.getElementById("customSelector").value = newsMap.get(activeTab.url);
+    })
+}
+
+function hightlightPage(data,selector) {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"action": "highlight_page", "selector": selector}, response => {
+        chrome.tabs.sendMessage(activeTab.id, {"action": "highlight_page", "data": data, "selector": selector}, response => {
             if(response.message === 'done') {
                 console.log('highlighted done');
             }
@@ -65,7 +91,7 @@ function unhightlightPage(selector) {
     });
 }
 
-function analysePage(selector, highlight=false) {
+function analysePage(selector) {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, {"action": "analyse_page", "selector": selector}, response => {
@@ -105,6 +131,8 @@ function analysePage(selector, highlight=false) {
 
                 const element = document.querySelector("#message");
                 element.innerHTML = `<p >Good: ${good.length}</p> <p >Neutral: ${neutral.length}</p> <p >Bad: ${bad.length}</p> <p>Overall rating: ${rating} </p>`;
+
+                this.hightlightPage(response.data.results, selector);
             }
         });
     });
